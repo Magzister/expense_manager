e6642ba1dc07fde19dfa17bc719a1658b7bddc79
Daniil Kulbatski <daniil.kulbackiy@gmail.com>
2022-12-05 14:43:42 +0300

    Add swagger.

--- app/app/settings.py
+++ app/app/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'celery',
     'django_celery_beat',
     'core',
--- app/app/urls.py
+++ app/app/urls.py
@@ -4,9 +4,24 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title='Expense manager API',
+        default_version='1.0.0',
+        description='API documentation for expense manager app'
+    ),
+    public=True
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include('core.urls')),
+    path('api/v1/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema'),
 ]
 
 if settings.DEBUG:
